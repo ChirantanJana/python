59698d4fe6279d5ad7a7885bd07cd09500aa9ba1
ChirantanJana <77533458+ChirantanJana@users.noreply.github.com>
2021-08-31 11:55:50 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,12 @@
+import os
+
+x=["dog","cat","rabbit"]
+y=[0,1]
+z=[3,6,9,12]
+a=['A','B','C','D','E']
+for i in range(3):
+	for j in range(2):
+		for k in range(4):
+			for m in range(5):
+				print(x[i],y[j],z[k],a[m])
+				
